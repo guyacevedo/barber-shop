ea9b9ebea590fa864229b911eed932ddeb2d954c
guyacevedo <guy.acevedoa@gmail.com>
2025-12-06 00:24:34 -0500

    Added fast-login-card component into login page

---
+++ src/app/features/auth/components/fast-login-card/fast-login-card.component.ts
@@ -0,0 +1,107 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  signal,
+  ElementRef,
+  effect,
+} from '@angular/core';
+import { FirebaseUserService } from '../../../../services/firebase/firebase-user.service';
+import { AuthFacade } from '../../auth.facade';
+import { UserBase } from '../../../../core/models';
+import { MockUserCacheService } from '../../services/mock-user-cache.service';
+import { CloudinaryService } from '../../../../services/cloudinary/cloudinary.service';
+import { MOCK_USERS } from '../../mocks/mock-users';
+import { SvgIconComponent } from "../../../../shared/icons/svg-icon.component";
+
+@Component({
+  selector: 'app-fast-login-card',
+  imports: [SvgIconComponent],
+  templateUrl: './fast-login-card.component.html',
+  styleUrl: './fast-login-card.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FastLoginCardComponent {
+  private userService = inject(FirebaseUserService);
+  private cache = inject(MockUserCacheService);
+  authFacade = inject(AuthFacade);
+  private readonly cloudinaryService = inject(CloudinaryService);
+  private elementRef = inject(ElementRef);
+
+  users = signal<(UserBase & { password: string })[]>([]);
+  showUsers = signal(false);
+
+  readonly defaultProfilePictureUrl =
+    this.cloudinaryService.defaultProfilePictureUrl;
+
+  constructor() {
+    if (this.cache.loaded()) {
+      this.users = this.cache.users;
+      this.showUsers.set(true);
+    } else {
+      this.loadMockUsers();
+    }
+
+    // Effect to handle image loading animations
+    effect(() => {
+      // Rerun when the users change
+      this.users();
+
+      setTimeout(() => {
+        if (typeof window !== 'undefined' && typeof document !== 'undefined') {
+          const images: NodeListOf<HTMLImageElement> =
+            this.elementRef.nativeElement.querySelectorAll('.img-fade-in');
+          images.forEach((image) => {
+            // Only add listener if it's not already loaded
+            if (image && !image.classList.contains('is-loaded')) {
+              if (image.complete) {
+                image.classList.add('is-loaded');
+              } else {
+                image.addEventListener(
+                  'load',
+                  () => {
+                    image.classList.add('is-loaded');
+                  },
+                  { once: true }
+                );
+              }
+            }
+          });
+        }
+      });
+    });
+  }
+
+  async loadMockUsers() {
+    console.log('Load Mock Users Started');
+    const userPromises = MOCK_USERS.map(async (mock) => {
+      const user = await this.userService.getUserById(mock.mockId);
+      return user ? { ...user, password: mock.mockPassword } : null;
+    });
+    const users = (await Promise.all(userPromises)).filter(
+      Boolean
+    ) as (UserBase & { password: string })[];
+
+    this.users.set(users);
+    this.cache.setUsers(users);
+    if (this.users().length > 0) this.showUsers.set(true);
+  }
+
+  async fastLogin(user: UserBase & { password: string }) {
+    console.log('Fast Login User Started with data: ', {
+      Email: user.email,
+      Password: user.password,
+    });
+    await this.authFacade.login(user.email, user.password);
+  }
+
+  getProfilePictureUrl(user: UserBase): string {
+    if (user && user.profilePictureUrl) {
+      return this.cloudinaryService.getTransformedUrl(
+        user.profilePictureUrl,
+        'w_60,h_60,c_fill,g_face,f_webp'
+      );
+    }
+    return this.defaultProfilePictureUrl;
+  }
+}
--- src/app/features/auth/pages/login-page/login-page.component.ts
+++ src/app/features/auth/pages/login-page/login-page.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component, effect, inject } from '@angular/core';
 import { LoginFormComponent } from '../../components/login-form/login-form.component';
+import { FastLoginCardComponent } from "../../components/fast-login-card/fast-login-card.component";
 @Component({
   selector: 'app-login-page',
-  imports: [LoginFormComponent],
+  imports: [LoginFormComponent, FastLoginCardComponent],
   templateUrl: './login-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
